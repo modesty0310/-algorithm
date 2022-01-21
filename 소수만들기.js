2d57e7890a9d2979735231bad15dcf16f17c04b4
modesty0310 <rlatprua4@gmail.com>
2022-01-21 20:17:25 +0900

    프로그래머스 소수만들기

---
+++ 소수만들기.js
@@ -0,0 +1,22 @@
+function solution(nums) {
+  let answer = 0;
+  const len = nums.length;
+
+  for(let i =0; i < len; i++){
+    for(let j =i+1; j < len; j++){
+      for(let k =j+1; k < len; k++){
+        const sum = nums[i] + nums[j] + nums[k];
+        if(isPrime(sum)) answer++;
+      }
+    }
+  }
+
+  function isPrime(sum){
+    for(let i = 2; i < sum; i++){
+      if(sum % i == 0) return false;
+    }
+    return true;
+  };
+
+  return answer;
+}
