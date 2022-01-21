a3abe57ec2d6b3a67011283f0d17cd7333ca7d30
modesty0310 <rlatprua4@gmail.com>
2022-01-21 21:06:54 +0900

    프로그래머스 모의고사

---
+++ 모의고사.js
@@ -0,0 +1,38 @@
+function solution(answers) {
+  let answer = [];
+  const first = [1,2,3,4,5];
+  const second = [2, 1, 2, 3, 2, 4, 2, 5,];
+  const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let scores = [0,0,0]
+
+  answers.map((a,i) => {
+    checkAnswer(a,i);
+  });
+
+  let maxNum = Math.max(...scores);
+  console.log(maxNum);
+
+  for(let i = 0; i < 3; i++){
+    if(scores[i] == maxNum){
+      answer.push(i+1)
+    }
+  }
+
+
+  function checkAnswer(num, index) {
+    if(num == first[index%(first.length+1)]){
+      scores[0]++;
+    }
+    if(num == second[index%(second.length+1)]){
+      scores[1]++;
+    }
+    if(num == third[index%(third.length+1)]){
+      scores[2]++;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([1,2,3,4,5])); 
+console.log(solution([1,3,2,4,2]));
